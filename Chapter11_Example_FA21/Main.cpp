ef68e00d1b8d701ee5e933c793ab333962fb2c67
BuschMath <busch@iowacentral.edu>
2021-11-10 10:21:21 -0600

    Using Array

--- Chapter11_Example_FA21/Main.cpp
+++ Chapter11_Example_FA21/Main.cpp
@@ -5,25 +5,46 @@
 
 using namespace std;
 
+void displayArray(double grades[], int arraySize);		// Display array
+void passArray(double grades[], int arraySize);			// Change to passing grades and display array
+
 int main()
 {
-	double studentGrades[10];
 	srand(unsigned int(time(NULL)));
+	double shortArray[3] = { 0, 1, 2 };
+	double studentGrades[10] = { 0.987, 0.789, rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX),
+		rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX),  rand() / double(RAND_MAX),
+		rand() / double(RAND_MAX) };
 
-	studentGrades[0] = 0.987;
-	studentGrades[1] = 0.789;
+	displayArray(studentGrades, 10);
+	passArray(studentGrades, 10);
 
-	for (int i = 2; i < 10; i++)
-	{
-		studentGrades[i] = rand() / double(RAND_MAX);
-	}
+	double* ptr_shortArray = shortArray;
+	ptr_shortArray = &shortArray[0];
 
-	for (int i = 0; i < 10; i++)
-	{
-		cout << fixed << setprecision(1) << "Student " << i + 1 << ": " << studentGrades[i] * 100 << "%\n";
-	}
+	displayArray(ptr_shortArray, 3);
 
 	cout << endl;
 
 	return 0;
+}
+
+void displayArray(double grades[], int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		cout << fixed << setprecision(1) << "Student " << i + 1 << ": " << grades[i] * 100 << "%\n";
+	}
+	cout << endl;
+}
+
+void passArray(double grades[], int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (grades[i] < 0.7)
+			grades[i] = 0.7;
+	}
+
+	displayArray(grades, arraySize);
 }
