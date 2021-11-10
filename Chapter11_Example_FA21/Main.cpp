0a30a9b21b699ff8a3d1fe46b82bb409eb4bf9c3
BuschMath <busch@iowacentral.edu>
2021-11-10 10:23:45 -0600

    Struct array

--- Chapter11_Example_FA21/Main.cpp
+++ Chapter11_Example_FA21/Main.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
-#include <time.h>
-#include <stdlib.h>
-#include <iomanip>
+#include <string>
 
 using namespace std;
 
-void displayArray(double grades[], int arraySize);		// Display array
-void passArray(double grades[], int arraySize);			// Change to passing grades and display array
+enum gradeType { A, B, C, D, F };
+
+struct studentRecord
+{
+	string firstName;
+	string lastName;
+	string className;
+	gradeType examGrades[4];
+	gradeType finalGrade;
+};
 
 int main()
 {
-	srand(unsigned int(time(NULL)));
-	double shortArray[3] = { 0, 1, 2 };
-	double studentGrades[10] = { 0.987, 0.789, rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX),
-		rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX),  rand() / double(RAND_MAX),
-		rand() / double(RAND_MAX) };
+	studentRecord classRecord[20];
 
-	displayArray(studentGrades, 10);
-	passArray(studentGrades, 10);
+	classRecord[0].firstName = "John";
+	classRecord[0].lastName = "Doe";
+	classRecord[0].className = "Introduction to Statistics";
+	classRecord[0].finalGrade = B;
 
-	double* ptr_shortArray = shortArray;
-	ptr_shortArray = &shortArray[0];
+	for (int i = 0; i < 4; i++)
+	{
+		classRecord[0].examGrades[i] = B;
+	}
 
-	displayArray(ptr_shortArray, 3);
-
-	cout << endl;
+	cout << classRecord[0].firstName + " " + classRecord[0].lastName << endl;
 
 	return 0;
-}
-
-void displayArray(double grades[], int arraySize)
-{
-	for (int i = 0; i < arraySize; i++)
-	{
-		cout << fixed << setprecision(1) << "Student " << i + 1 << ": " << grades[i] * 100 << "%\n";
-	}
-	cout << endl;
-}
-
-void passArray(double grades[], int arraySize)
-{
-	for (int i = 0; i < arraySize; i++)
-	{
-		if (grades[i] < 0.7)
-			grades[i] = 0.7;
-	}
-
-	displayArray(grades, arraySize);
 }
