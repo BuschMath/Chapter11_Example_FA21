019fd4ae563cbbd5db990ca19a521bda7db28be6
BuschMath <busch@iowacentral.edu>
2021-11-15 10:08:10 -0600

    Working with 2D array

--- Chapter11_Example_FA21/Main.cpp
+++ Chapter11_Example_FA21/Main.cpp
@@ -2,66 +2,82 @@
 
 using namespace std;
 
-void functionI(int i_array[][3]);
+const int numRows = 3;
+const int numCols = 4;
 
-typedef double Array2D[2][3];			
-void functionD(Array2D d_array);
+typedef double Array2D[numRows][numCols];
+void RowSum(Array2D r_array, double* arrayRowSum);
+void ColSum(Array2D c_array, double* arrayColSum);
+
 
 int main()
 {
-	int i_array[2][3];
+	double i_array[numRows][numCols];
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < numRows; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < numCols; j++)
 		{
-			i_array[i][j] = 0;
+			i_array[i][j] = i + j;
 		}
 	}
 
-	functionI(i_array);
+	double arrayRowSum[numRows];
+
+	RowSum(i_array, arrayRowSum);
+
+	cout << "Returned Row Sum:\n";
+	for (int i = 0; i < numRows; i++)
+	{
+		cout << arrayRowSum[i] << endl;
+	}
+	cout << endl;
 
 	Array2D d_array;
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < numRows; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < numCols; j++)
 		{
 			d_array[i][j] = 1;
 		}
 	}
 
-	functionD(d_array);
+	double arrayColSum[numCols];
+	ColSum(d_array, arrayColSum);
 
 	return 0;
 }
 
-void functionI(int i_array[][3])
+void RowSum(Array2D i_array, double* sum)
 {
-	cout << "functionI:\n";
+	cout << "Row Sum:\n";
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < numRows; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		sum[i] = 0;
+		for (int j = 0; j < numCols; j++)
 		{
-			cout << i_array[i][j] << " ";
+			sum[i] += i_array[i][j];
 		}
-		cout << endl;
+		cout << sum[i] << endl;
 	}
 	cout << endl;
 }
 
-void functionD(Array2D d_array)
+void ColSum(Array2D d_array, double* sum)
 {
-	cout << "functionD:\n";
+	cout << "Column Sum:\n";
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < numCols; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		sum[i] = 0;
+		for (int j = 0; j < numRows; j++)
 		{
-			cout << d_array[i][j] << " ";
+			sum[i] += d_array[j][i];
+
 		}
-		cout << endl;
+		cout << sum[i] << " ";
 	}
-	cout << endl;
+	cout << endl << endl;
 }
